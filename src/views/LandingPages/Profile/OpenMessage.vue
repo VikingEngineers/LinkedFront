9cc0a83aaaf9bfab703144f732bc8c4dda56c4b1
irchen4 <81490453+irchen4@users.noreply.github.com>
2023-06-09 20:15:49 +0300

    вкладки для входящих и исходящих

--- src/views/LandingPages/Profile/OpenMessage.vue
+++ src/views/LandingPages/Profile/OpenMessage.vue
@@ -9,20 +9,6 @@ const messageData = ref([]);
 const debugText = ref('');
 const searchResultUsers = ref([]);
 
-// // Axios request and response interceptors
-// axios.interceptors.request.use((request) => {
-//   //debugText.value += '\n\nRequest:\n' + JSON.stringify(request, null, 2);
-//   return request;
-// });
-
-// axios.interceptors.response.use((response) => {
-//   // debugText.value += '\n\nResponse:\n' + JSON.stringify(response, null, 2);
-//   return response;
-// }, (error) => {
-//   // debugText.value += '\n\nResponse Error:\n' + JSON.stringify(error.toJSON(), null, 2);
-//   return Promise.reject(error);
-// });
-
 
 
 const getMessages = async () => {
@@ -70,59 +56,17 @@ onMounted(async() => {
     <NavbarDefault />
     <p >{{ debugText }}</p>
     <div class="profile-container" :style="{fontWeight: '900',  fontFamily: 'monospace' }">
-      <h2 :style="{ fontSize: '27px', fontWeight: '900',  fontFamily: 'PressStart2P, sans-serif', marginBottom:'5%' }">Мои сообщения</h2>
-      <RouterLink :style="{fontFamily: 'PressStart2P, sans-serif', fontSize: '14px'}"
-                  :to="{ name: 'send-message' }"
-                  class="btn_link"
-                  >
-                  Написать сообщение
-                </RouterLink> 
-        <p :style="{ fontSize: '24px'}">{{ messageData.email }}</p>
-
-        <div class="tabs">
-          <div id="content-1">
-            <div v-for = "message in messageData" :key="message.id" class="one_inbox" :style="{ fontSize: '12px', fontWeight: '500',  fontFamily: 'PressStart2P, sans-serif' }">
-          
-              <p :style="{ fontSize: '12px'}">От: {{ findUsername(message.sender) }}</p>
-              <p :style="{ fontSize: '12px'}">Кому: {{ findUsername(message.recipient) }} </p>
-              <p :style="{ fontSize: '12px'}">Тема: {{ message.subject }}</p>
-              <p :style="{ fontSize: '12px'}">Сообщение: {{ message.body }}</p>
-            </div>
+  
     
-          </div>
-          <div id="content-2">
-            <div v-for = "message in messageData" :key="message.id" class="one_inbox" :style="{ fontSize: '12px', fontWeight: '500',  fontFamily: 'PressStart2P, sans-serif' }">
-          
-              <p :style="{ fontSize: '12px'}">От: {{ findUsername(message.sender) }}</p>
-              <p :style="{ fontSize: '12px'}">Кому: {{ findUsername(message.recipient) }} </p>
-              <p :style="{ fontSize: '12px'}">Тема: {{ message.subject }}</p>
-              <p :style="{ fontSize: '12px'}">Сообщение: {{ message.body }}</p>
-            </div>
-          </div>
-    
-          <div class="tabs__links">
-            <a href="#content-1">Входящие</a>
-            <a href="#content-2">Исходящие</a>
-          </div>
-        </div>
-
-<!--         <div v-for = "message in messageData" :key="message.id" class="one_inbox" :style="{ fontSize: '12px', fontWeight: '500',  fontFamily: 'PressStart2P, sans-serif' }">
-          
-          <p :style="{ fontSize: '12px'}">От: {{ findUsername(message.sender) }}</p>
-          <p :style="{ fontSize: '12px'}">Кому: {{ findUsername(message.recipient) }} </p>
-          <p :style="{ fontSize: '12px'}">Тема: {{ message.subject }}</p>
-          <p :style="{ fontSize: '12px'}">Сообщение: {{ message.body }}</p>
-        </div> -->
-
-
-      
     </div>
+
     <DefaultFooter />
   </template> 
 
 
 
 <style scoped>
+
 .tabs {
   display: flex;
   flex-direction: column;
@@ -193,8 +137,6 @@ onMounted(async() => {
 .tabs>div:target {
   display: block;
 }
-
-
   .btn_link {
     /* Adds some padding inside the button */
     padding: 10px;
@@ -232,6 +174,7 @@ onMounted(async() => {
     margin-right: 10%;
     box-sizing: border-box;
     width: 50%;
+    font-size: 18px;
   }
   
   .profile-container {
@@ -245,45 +188,16 @@ onMounted(async() => {
   align-items: center;
   text-align: center;
 }
-.text-container {
-  width: 90%;
-  margin-left: 9%;
-  background-color: #ffffff57;
-  border-radius: 10px;
-  text-align: left;
-  margin-bottom: 30px;
-}
-.social-link {
-  margin-left: 5%;
-  width: 100%;
-  flex-direction: row;
-  display: flex;
-  flex-wrap: wrap;
-  text-align: center;
-  }
-.social-link a{
-margin: 3% 4%;
-border-radius: 5px;
-border: 2px solid #4ea852e0;
-padding: 7px;
-}
+
 @media screen and (max-width: 992px) {
   .profile-container {
     width: 90%;
-  }
-  .text-container {
-    width: 70%;
-    margin: 5% 15%;
   }
 }
 
 @media screen and (max-width: 600px) {
   .profile-container {
     width: 90%;
-  }
-  .text-container {
-    width: 70%;
-    margin: 5% 15%;
   }
 }
 .profile-container img {
