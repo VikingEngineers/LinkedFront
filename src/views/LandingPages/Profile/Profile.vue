1c3244fa803f51e0fa9a60a7ffe95e527f5e86c2
FEARmeR <fearmer@gmail.com>
2023-06-21 14:21:25 +0100

    Ссылка "резюме" в профиле

--- src/views/LandingPages/Profile/Profile.vue
+++ src/views/LandingPages/Profile/Profile.vue
@@ -86,6 +86,7 @@ onMounted(async() => {
         <a :href="`${profileData.social_vk}`" target="_blank" >VK</a>
         <a :href="`${profileData.social_youtube}`" target="_blank" >YouTube</a>
         <a :href="`${profileData.social_website}`" target="_blank" >Мой сайт</a>
+        <a :href="'/profiletoprint/' + profileData.id" target="_blank" >Резюме</a>
         <router-link :style="{fontSize: '10px'}"
                   :to="{ name: 'message-to', params: { id: route.params.id } }"
                   class="btn_link"
